a072d5116c44e753523f56ebc295861a9904070c
Mukeswar <vasanamukeswar@gmail.com>
2025-09-01 12:16:05 +0530

    ec2 instance

---
+++ ec2-instance-structure/output.tf
@@ -0,0 +1,6 @@
+output "EC2_public_ip" {
+    value = aws_instance.linux_VM.public_ip
+}
+output "EC2_private_ip" {
+    value = aws_instance.linux_VM.private_ip
+}
---
+++ ec2-instance-structure/vars.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+    description = "AMI Id"
+    default = "ami-0c4fc5dcabc9df21d"
+}
+variable "instance_type" {
+    description = "Instance type"
+    default = "t3.micro"
+}
